0bce4bda6d93012180441ae64ed53cb1618e11ef
Nguyen Duc Lan <s.gintoki710@gmail.com>
2024-10-10 20:43:46 +0700

    replace Controller and Service class with RepositoryRestResource, auto generate entrypoint, easy to sort and paging data by query para in url

--- src/main/java/com/example/demo/dao/StudentRepository.java
+++ src/main/java/com/example/demo/dao/StudentRepository.java
@@ -3,7 +3,9 @@ package com.example.demo.dao;
 import com.example.demo.entity.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
 }
